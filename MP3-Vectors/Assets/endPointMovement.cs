4bedc9a235ab6ba40c98c1dce088f1ce4a5aead7
Cirnol <cn6495@yahoo.com>
2020-10-22 14:52:12 -0700

    Endpoint Movement

---
+++ MP3-Vectors/Assets/endPointMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endPointMovement : MonoBehaviour
+{
+    private Vector3 target;
+    private int wallA;
+    private int wallB;
+    public GameObject epA;
+    public GameObject epB;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wallA = (1 << 8);
+        wallB = (1 << 9);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            // Checks for object to be deleted before allowing movement
+
+            if (Physics.Raycast(ray, out hit, 1000.0f, wallA))
+            {
+                target = hit.point; // Obtain the coordinates
+                epA.transform.position = target; // Object moves to those coordinates
+            }
+
+            if (Physics.Raycast(ray, out hit, 1000.0f, wallB))
+            {
+                target = hit.point; // Obtain the coordinates
+                epB.transform.position = target; // Object moves to those coordinates
+            }
+        }
+    }
+}
