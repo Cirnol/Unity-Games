227992455ea86ad06639f5ad7a70cf4239ece829
Cirnol <cn6495@yahoo.com>
2020-10-29 22:24:32 -0700

    Redo the Reflection

--- MP3-Vectors/Assets/Scripts/travellingBallBehavior.cs
+++ MP3-Vectors/Assets/Scripts/travellingBallBehavior.cs
@@ -11,12 +11,16 @@ public class travellingBallBehavior : MonoBehaviour
     Vector3 move;
     float health;
 
+    public GameObject barrier;
+
     // Start is called before the first frame update
     void Start()
     {
         offset.Normalize();
         move = new Vector3(offset.x, offset.y, offset.z) * Time.deltaTime * setSpeed;
         health = lifeSpan;
+
+        barrier = GameObject.Find("Barrier");
     }
 
     // Update is called once per frame
@@ -27,5 +31,25 @@ public class travellingBallBehavior : MonoBehaviour
         health -= Time.deltaTime;
         if (health <= 0)
             Destroy(gameObject);
+
+        // Reflection
+        Vector3 V = (this.transform.position - barrier.transform.position);
+        V.Normalize();
+
+        Vector3 n = -barrier.transform.forward;
+        Vector3 center = barrier.transform.localPosition;
+        float d = Vector3.Dot(n, center);
+
+        float isBehindBarrier = Vector3.Dot(n, V);
+
+        if(isBehindBarrier < 0)
+        {
+            move = 2 * (Vector3.Dot(-move, n)) * n - (-move);
+        }
     }
+
+
+
+
 }
+
