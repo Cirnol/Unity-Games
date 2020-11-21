20feab00ac213e0df3b722570d0dc20d702e8ca7
Cirnol <cn6495@yahoo.com>
2020-11-20 22:47:43 -0800

    Update CameraManipulation.cs

--- MP4-Hierarchy/Assets/Source/Model/CameraManipulation.cs
+++ MP4-Hierarchy/Assets/Source/Model/CameraManipulation.cs
@@ -169,11 +169,12 @@ public class CameraManipulation : MonoBehaviour {
         Vector3 newCameraPos = r.MultiplyPoint(transform.localPosition);
         transform.localPosition = newCameraPos;
 
-        // Replaces built-in LookAt function
+        // This replaces built-in LookAt function
         Vector3 V = LookAtPosition.localPosition - transform.localPosition;
         Vector3 W = Vector3.Cross(-V, Vector3.up);
         Vector3 U = Vector3.Cross(W, -V);
-        //transform.localRotation = Quaternion.LookRotation(V, U);
+
+        //transform.localRotation = Quaternion.LookRotation(V, U); // This gets replaced by the next 3 lines
         transform.localRotation = Quaternion.FromToRotation(Vector3.up, U);
         Quaternion alignU = Quaternion.FromToRotation(transform.forward, V);
         transform.localRotation = alignU * transform.localRotation;
